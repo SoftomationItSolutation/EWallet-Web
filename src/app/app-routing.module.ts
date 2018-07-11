a32dbf9d6d8defef381b505ebdcb660cf374d504
hemantpundir87 <39514591+hemantpundir87@users.noreply.github.com>
2018-07-12 00:28:07 +0530

    update
    hemant

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './userlogin/login/login.component';
+import { ForgetPasswordComponent } from './userlogin/forget-password/forget-password.component';
+import { RegistrationComponent } from './userlogin/registration/registration.component';
+
+const appRoutes: Routes = [ 
+  { path: 'dashborad', component: DashboardComponent }, 
+  { path: '', component: LoginComponent }, 
+  { path: 'forget', component: ForgetPasswordComponent}, 
+  { path: 'registration', component: RegistrationComponent}, 
+  { path: 'login', redirectTo: '', pathMatch: 'full' }, 
+  { path: '**', component: PageNotFoundComponent } ];
+  export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes ,{useHash: true});
+@NgModule({
+  imports: [CommonModule,routing],
+  exports: [RouterModule],
+  declarations: []
+})
+
+
+  
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './routing/app-routing.module';
-
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import {  MatIconModule, MatRippleModule,MatSidenavModule,  MatMenuModule,   MatButtonModule,    MatCardModule,  MatFormFieldModule,  MatInputModule,  MatTabsModule,  MatTableModule,  MatPaginatorModule,   MatSortModule,  MatSelectModule,  MatDialogModule,  MatTreeModule,  MatProgressSpinnerModule,  MatListModule,  MatTooltipModule,  MatGridListModule,  MatCheckboxModule,MatStepperModule, MatToolbarModule} from '@angular/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-
 import { LoginComponent } from './userlogin/login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RegistrationComponent } from './userlogin/registration/registration.component';
